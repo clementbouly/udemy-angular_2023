32c38c12e7e8f6c3f35f4bfcea6d3d1b9f1770fa
clementbouly <clementbouly@hotmail.fr>
2023-05-04 15:46:14 +0200

    core(AngularVersion): update to Angular 16 and test signals

--- angular-formation/src/app/recipes/recipes.component.ts
+++ angular-formation/src/app/recipes/recipes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal, effect, signal } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 @Component({
@@ -8,8 +8,19 @@ import { Recipe } from './recipe.model';
 })
 export class RecipesComponent {
   selectedRecipe: Recipe;
+  count: WritableSignal<number> = signal(0);
+
+  constructor() {
+    effect(() => {
+      console.log(`The count is: ${this.count()}`);
+    });
+  }
 
   onRecipeSelected(recipe: Recipe) {
     this.selectedRecipe = recipe;
+  }
+
+  testSignal() {
+    this.count.update((value) => value + 1);
   }
 }
